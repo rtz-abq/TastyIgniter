1908c8f212698a184eb3b7dd2356c8d0eb6a1c7c
rtz-abq <250626947+rtz-abq@users.noreply.github.com>
2026-02-06 15:58:16 -0700

    back to default

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = '/home/stmweb/stm/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -31,7 +31,7 @@ if (file_exists($maintenance = '/home/stmweb/stm/storage/framework/maintenance.p
 |
 */
 
-require '/home/stmweb/stm/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require '/home/stmweb/stm/vendor/autoload.php';
 |
 */
 
-$app = require_once '/home/stmweb/stm/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
